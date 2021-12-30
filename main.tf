d665762f51cdf4ce68f3242a13dce6fd0cf137f7
jsolarz <jsolarz@google.com>
2021-12-30 16:14:25 +0100

    Add full lifecycle for Artifact Registry, Docker, Image and second revision creation

--- main.tf
+++ main.tf
@@ -4,7 +4,17 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "2.15.0"
     }
+
+    google = {
+      version = "3.90.1"
+    }
   }
+}
+
+locals {
+  service-repo = "https://github.com/jeremysolarz/cloud-run-hello.git"
+  service-name = "cloud-run-hello"
+  region       = "us-central1"
 }
 
 provider "google" {
@@ -34,35 +44,41 @@ resource "google_artifact_registry_repository" "my-repo" {
 
   project                  = var.project_id
 
-  location = "us-central1"
-  repository_id = "cloud-run-hello"
+  location = local.region
+  repository_id = ${local.service-name}
   description = "example docker repository"
   format = "DOCKER"
 }
 
-# resource "null_resource" "setup" {
-#   provisioner "local-exec" {
-#     command = "gcloud auth configure-docker us-central1-docker.pkg.dev"
-#   }
-# }
+resource "null_resource" "setup" {
+   provisioner "local-exec" {
+     command = "gcloud auth configure-docker ${local.region}-docker.pkg.dev"
+   }
+}
 
 resource "null_resource" "update_image" {
+
+  depends_on = [
+    null_resource.setup
+  ]
 
   triggers = {
     always_run = "${timestamp()}"
   }
 
 
-#   provisioner "local-exec" {
-#     command = <<EOF
-#       cd ..
-#       docker build -t \
-#         us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello \
-#         -f Dockerfile .
-#       docker push \
-#         us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello:latest 
-# EOF
-#   }
+  provisioner "local-exec" {
+     command = <<EOF
+       rm -rf service-repo
+       git clone ${local.service-repo} service-repo
+       cd service-repo
+       docker build -t \
+         ${local.region}-docker.pkg.dev/${var.project_id}/${local.service-name}/${local.service-name} \
+         -f Dockerfile .
+       docker push \
+         ${local.region}-docker.pkg.dev/${var.project_id}/${local.service-name}/${local.service-name}
+EOF
+  }
 }
 
 data "google_client_config" "default" {}
@@ -70,9 +86,9 @@ data "google_client_config" "default" {}
 provider "docker" {
 
   host = "ssh://localhost:60006"
-
+  
   registry_auth {
-    address  = "us-central1-docker.pkg.dev"
+    address  = "${local.region}-docker.pkg.dev"
     username = "oauth2accesstoken"
     password = data.google_client_config.default.access_token
   }
@@ -81,91 +97,12 @@ provider "docker" {
 data "docker_registry_image" "container_image" {
 
   depends_on = [
-    # null_resource.setup,
+    null_resource.setup,
     null_resource.update_image
   ]
 
-  name = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello"
+  name = "${local.region}-docker.pkg.dev/${var.project_id}/${local.service-name}/${local.service-name}"
 }
-
-# module "cloud_run_green" {
-#   source  = "GoogleCloudPlatform/cloud-run/google"
-#   version = "~> 0.1.1"
-
-#   depends_on = [
-#   #   null_resource.setup,
-#      data.docker_registry_image.container_image
-#   ]
-
-#   # Required variables
-#   project_id             = var.project_id
-#   service_name           = "cloud-run-hello"
-#   location               = "us-central1"
-#   image                  = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello@${data.docker_registry_image.container_image.sha256_digest}"
-#   members                = [
-#     "allUsers"
-#   ]
-  
-#   env_vars = [
-#     {
-#       "name" : "COLOR",
-#       "value": "#66FF99"
-#     }
-#   ]
-
-#   generate_revision_name = false
-
-#   traffic_split          = [
-#     {
-#       "latest_revision": false,
-#       "percent": 100,
-#       "revision_name": "cloud-run-hello-green"
-#     }
-#   ]
-
-# }
-
-# module "cloud_run_blue" {
-#   source  = "GoogleCloudPlatform/cloud-run/google"
-#   version = "~> 0.1.1"
-
-#   depends_on = [
-#   #   null_resource.setup,
-#      data.docker_registry_image.container_image,
-#      module.cloud_run_green
-#   ]
-
-#   # Required variables
-#   project_id             = var.project_id
-#   service_name           = "cloud-run-hello"
-#   location               = "us-central1"
-#   image                  = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello@${data.docker_registry_image.container_image.sha256_digest}"
-#   members                = [
-#     "allUsers"
-#   ]
-#   generate_revision_name = false
-  
-#   env_vars = [
-#     {
-#       "name" : "COLOR",
-#       "value": "#D5F3FE"
-#     }
-#   ]
-
-#   traffic_split          = [
-#     {
-#       "latest_revision": false,
-#       "percent": 75,
-#       "revision_name": "cloud-run-hello-blue"
-#     },
-#     {
-#       "latest_revision": false,
-#       "percent": 25,
-#       "revision_name": "cloud-run-hello-green"
-#     }
-#   ]
-
-# }
 
 data "google_iam_policy" "noauth" {
   binding {
@@ -176,55 +113,29 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-# gcloud run deploy cloud-run-hello --image us-central1-docker.pkg.dev/$(gcloud config get-value project)/cloud-run-hello/cloud-run-hello@sha256:eeb7416182d270b1ee9831004fb46b790d480fad600d0597aad0ddf27382b980 \
-#     --set-env-vars COLOR=#66FF99 --revision-suffix green --allow-unauthenticated
+resource "null_resource" "green" {
 
-# resource "google_cloud_run_service" "green" {
-#   name     = "cloud-run-hello"
-#   location = "us-central1"
-
-#   template {
-#     spec {
-#       containers {
-#         # sha256:eeb7416182d270b1ee9831004fb46b790d480fad600d0597aad0ddf27382b980
-#         image = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello@${data.docker_registry_image.container_image.sha256_digest}"
-#         env {
-#           name = "COLOR"
-#           value = "#66FF99"
-#         }
-#       }
-#     }
-
-#     metadata {
-#       name = "cloud-run-hello-green"
-#     }
-#   }
-
-#   autogenerate_revision_name = false
-
-#   traffic {
-#     percent         = 100
-#     latest_revision = false
-#     revision_name   = "cloud-run-hello-green" 
-#   }
-# }
-
-# resource "google_cloud_run_service_iam_policy" "green" {
-#   location    = google_cloud_run_service.green.location
-#   project     = google_cloud_run_service.green.project
-#   service     = google_cloud_run_service.green.name
-
-#   policy_data = data.google_iam_policy.noauth.policy_data
-# }
+   provisioner "local-exec" {
+     command = <<EOF
+     gcloud run deploy ${local.service-name} \
+     --image ${local.region}-docker.pkg.dev/${var.project_id}/${local.service-name}/${local.service-name}@${data.docker_registry_image.container_image.sha256_digest} \
+     --set-env-vars COLOR=#66FF99 --revision-suffix green --allow-unauthenticated
+EOF
+   }
+}
 
 resource "google_cloud_run_service" "blue" {
-  name     = "cloud-run-hello"
-  location = "us-central1"
+  name     = local.service-name
+  location = local.region
+
+  depends_on = [
+    null_resource.green
+  ]
 
   template {
     spec {
       containers {
-        image = "us-central1-docker.pkg.dev/${var.project_id}/cloud-run-hello/cloud-run-hello@${data.docker_registry_image.container_image.sha256_digest}"
+        image = "${local.region}-docker.pkg.dev/${var.project_id}/${local.service-name}/${local.service-name}@${data.docker_registry_image.container_image.sha256_digest}"
         env {
           name = "COLOR"
           value = "#D5F3FEf"
@@ -233,22 +144,22 @@ resource "google_cloud_run_service" "blue" {
     }
 
     metadata {
-      name = "cloud-run-hello-blue"
+      name = "${local.service-name}-blue"
     }
   }
 
   autogenerate_revision_name = false
 
   traffic {
-    percent         = 75
+    percent         = 25
     latest_revision = false
-    revision_name   = "cloud-run-hello-green" 
+    revision_name   = "${local.service-name}-blue" 
   }
 
   traffic {
-    percent         = 25
+    percent         = 75
     latest_revision = false
-    revision_name   = "cloud-run-hello-blue" 
+    revision_name   = "${local.service-name}-green" 
   }
 }
 
